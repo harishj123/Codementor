f4150d323683a7e6d67b8e8dd156bf049136a0ee
Harish J <162663198+harishj123@users.noreply.github.com>
2026-07-17 20:18:47 +0530

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
